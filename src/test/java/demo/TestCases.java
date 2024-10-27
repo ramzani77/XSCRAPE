7b381c6aad45868b446fca86d17f735986d33689
Sameer Ramzani <sameerrza77@gmail.com>
2024-10-28 02:42:25 +0530

    ME_QA_XSCRAPE_DATA_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,21 +1,30 @@
 package demo;
 
-import org.openqa.selenium.By;
+
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.logging.Level;
-// import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.time.Duration;
+
 import demo.wrappers.Wrappers;
+// import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class TestCases {
     ChromeDriver driver;
+    String url = "https://www.scrapethissite.com/pages/";
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+    WebDriverWait wait = new WebDriverWait(driver,  Duration.ofSeconds(10));
 
     /*
      * TODO: Write your tests here with testng @Test annotation. 
@@ -47,6 +56,34 @@ public class TestCases {
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+    }
+
+    @Test (priority = 1, description = "Test scraping Hockey team data", enabled = true)
+    public void testCase01() throws Exception{
+     System.out.println("Started : TestCase01");
+     //navigating to the website 
+     Wrappers.navigateToWebsite(driver,url);
+   
+
+     //Clicking on the heading
+     Wrappers.headingSelection(driver,"Hockey");
+     //printing the Teamname ,year, win %
+     Wrappers.teamData(driver);
+     System.out.println("End : TestCase01");
+    }
+
+    @Test (priority = 2, description = "Test scraping Oscar movie data", enabled = true)
+    public void testCase02() throws Exception{
+        System.out.println("Started : TestCase02");
+     //navigating to the website 
+     Wrappers.navigateToWebsite(driver,url);
+     
+     //Clicking on the  heading
+     Wrappers.headingSelection(driver,"Oscar");
+     Wrappers.scrapeOscarData(driver);
+     Wrappers.verifyFile();
+     
+
     }
 
     @AfterTest
